e04675d69ccb49dcb1d500bacbe993545acc3328
Noah Manaigre <nmanaigre2@academic.rrc.ca>
2025-10-21 17:50:36 -0500

    Finished step 5 and started step 6.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
 import express from "express";
 import loanRoutes from "./api/v1/routes/loanRoutes";
+import userRoutes from "./api/v1/routes/userRoutes";
+import adminRoutes from "./api/v1/routes/adminRoutes";
+import { accessLogger, errorLogger, consoleLogger } from "./api/v1/middleware/logger";
+import errorHandler from "./api/v1/middleware/errorHandler";
 
 const app = express();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(accessLogger);
+  app.use(errorLogger);
+} else {
+  app.use(consoleLogger);
+}
+
 app.use(express.json());
 app.use("/api/v1/loans", loanRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/admin", adminRoutes);
+app.use(errorHandler);
 
 export default app;

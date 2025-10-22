9cb76feca13b694047ecef311e56b7034ae54257
Noah Manaigre <nmanaigre2@academic.rrc.ca>
2025-10-21 22:20:19 -0500

    Fixing code and uploading more screenshots for postman

--- src/api/v1/routes/loanRoutes.ts
+++ src/api/v1/routes/loanRoutes.ts
@@ -1,18 +1,20 @@
 import express from "express";
+import { authenticate } from "../middleware/authenticate";
 import {
-  createLoan,
-  getLoans,
-  getLoanById,
-  approveLoan,
-  reviewLoan,
+    createLoan,
+    getLoans,
+    getLoanById,
+    approveLoan,
+    reviewLoan
 } from "../controllers/loanController";
 
 const router = express.Router();
 
 router.post("/", createLoan);
-router.get("/", getLoans);
+// router.get("/", getLoans);
 router.get("/:id", getLoanById);
 router.put("/:id/approve", approveLoan);
 router.post("/:id/review", reviewLoan);
+router.get("/", authenticate, getLoans);
 
 export default router;
